b409c22d4b42c1760bae205ff76d01f303410517
Mateo Allegre <mateo.allegre@etu.uca.fr>
2023-11-13 17:01:27 +0100

    Ajout du support du bouton

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
@@ -29,6 +30,27 @@ static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
 		      BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))
 };
+
+/////////////////////////// BUTTON ///////////////////////////////////////////////
+
+/*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
+static struct gpio_callback button_cb_data;
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	bt_input_set_button(1U);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
@@ -77,42 +99,42 @@ static struct bt_conn_auth_cb auth_cb_display = {
 	.cancel = auth_cancel,
 };
 
-static void input_notify(void)
-{
-	uint8_t button_pressed = bt_input_get_button();
-
-	if(button_pressed) {
-		button_pressed = 0U;
-	} else {
-		button_pressed = 1U;
-	}
-
-	bt_input_set_button(button_pressed);
-}
-
 int main(void)
 {
 	int err;
+
+	int ret;
+
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return 0;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return 0;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 	err = bt_enable(NULL);
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
 		return 0;
 	}
-
-	err = output_init();
-    if (err)
-    {
-        printk("Output service initialization failed (err %d)\n", err);
-        return 0;
-    }
-
-	err = system_init();
-    if (err)
-    {
-        printk("System service initialization failed (err %d)\n", err);
-        return 0;
-    }
 
 	bt_ready();
 
@@ -127,8 +149,8 @@ int main(void)
 		/* Update system debug information */
         system_set_debug("No errors");  // Set your debug message as needed
 
-		/* Button simulation */
-		input_notify();
+		// Initialisation du bouton à zéro
+		bt_input_set_button(0U);
 	}
 	return 0;
 }
